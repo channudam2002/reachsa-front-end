ac38789053c7a1f6057a78b84f65f2dc50390738
udam <channudam.2002@gmail.com>
2023-01-28 12:02:34 +0700

    fix-developerscreen-responsive

--- src/views/DeveloperView.vue
+++ src/views/DeveloperView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="w-full h-screen flex flex-col">
-        <div class="w-full h-3/6 text-white">
+  <div class="w-full md:h-screen min-h-screen flex flex-col">
+        <div class="w-full sm:h-3/6 h-[500px] text-white">
             <div class="relative w-full h-full overflow-hidden">
-                <video src="../assets/herovideo6.mp4" class="absolute top-0 left-0 w-full" autoplay loop></video>
+                <video src="../assets/herovideo6.mp4" class="absolute top-0 left-0 w-full h-full" autoplay loop></video>
                 <div class="w-full h-full absolute top-0 left-0 bg-gradient-to-b from-transparent to-black"></div>
-                <div class="flex flex-col space-y-10 p-36 w-full justify-center items-center h-full absolute top-0 left-0">
+                <div class="flex flex-col space-y-10 sm:p-36 p-10 py-24 w-full justify-end items-center h-full absolute top-0 left-0 text-center">
                     <div>
-                        <p class="text-6xl">Push your <span class="text-secondary-color">project</span> to real world of <span class="text-primary-color">IoT</span></p>
+                        <p class="sm:text-6xl text-3xl">Push your <span class="text-secondary-color">project</span> to real world of <span class="text-primary-color">IoT</span></p>
                     </div>
-                    <div class="p-4 px-8 rounded-full border text-3xl cursor-pointer hover:bg-primary-color hover:border hover:border-primary-color hover:bg-opacity-50">
+                    <div class="p-4 px-8 rounded-full border sm:text-3xl text-xl cursor-pointer hover:bg-primary-color hover:border hover:border-primary-color hover:bg-opacity-50">
                         <p>Go to Dashboard</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="w-full h-3/6 py-32 px-72 items-center justify-center space-x-20 flex flex-row text-white">
-            <div class="w-1/4 flex flex-col space-y-3 items-center border rounded-md p-5 hover:bg-white hover:text-black cursor-pointer">
+        <div class="w-full h-3/6 sm:py-32 sm:px-72 p-10 items-center justify-center sm:space-x-20 sm:space-y-0 flex sm:flex-row flex-col space-x-0 space-y-10 text-white">
+            <div class="sm:w-1/4 w-full flex flex-col space-y-3 items-center border rounded-md p-5 hover:bg-white hover:text-black cursor-pointer">
                 <div class="text-3xl">
                     <i class="fa-solid fa-book"></i>
                 </div>
@@ -32,7 +32,7 @@
 
                 </div>
             </div>
-            <div class="w-1/4 flex flex-col space-y-3 items-center border rounded-md p-5 hover:bg-white hover:text-black cursor-pointer">
+            <div class="sm:w-1/4 w-full flex flex-col space-y-3 items-center border rounded-md p-5 hover:bg-white hover:text-black cursor-pointer">
                 <div class="text-3xl">
                     <i class="fa-solid fa-video"></i>
                 </div>
