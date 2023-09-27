5d0c6afeba3753418ea0739f9a85d1320c8f80c4
channudam2002 <channudam.2002@gmail.com>
2023-09-27 11:12:12 +0700

    add chunk

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devtool: false,
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        minSize: 15000,
+        maxSize: 250000,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+      },
+    },
+  }
 })
